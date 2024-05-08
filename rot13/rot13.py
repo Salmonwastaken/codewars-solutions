bc2a697813c4c0772e251cee29e54e15f6108095
Salmonwastaken <34370061+Salmonwastaken@users.noreply.github.com>
2024-05-08 22:00:14 +0200

    rot13

---
+++ rot13/rot13.py
@@ -0,0 +1,26 @@
+# https://www.codewars.com/kata/530e15517bc88ac656000716
+# One liner, great fun for the whole family
+
+import string
+
+
+def rot13(message):
+    return "".join([string.ascii_letters[int((string.ascii_letters.index(letter) + 13) % 26)+26] if letter.isupper() else string.ascii_letters[int((string.ascii_letters.index(letter) + 13) % 26)] if letter in string.ascii_letters else for letter in message])
+
+    letters = string.ascii_letters
+
+    answer = ""
+
+    for letter in message:
+        if letter in letters:
+            letterIndex = int((letters.index(letter) + 13) % 26)
+            answer += (letters[letterIndex+26] if letter.isupper() else letters[letterIndex])
+        else:
+            answer += letter
+    return answer
+
+
+print(rot13("Test"))  # Grfg
+print(rot13("test"))  # grfg
+print(rot13("test!!"))  # grfg!!
+print(rot13("aAaA!!"))  # grfg!!
